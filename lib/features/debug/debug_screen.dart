dfba75917453c50da9be413602be5e5f15332f3a
Елена <99683487+eln02@users.noreply.github.com>
2025-07-04 23:47:46 +0300

    график счета на моках бэкап

--- lib/features/debug/debug_screen.dart
+++ lib/features/debug/debug_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yandex_school_homework/features/accounts/presentation/screens/diagram.dart';
 
 class DebugScreen extends StatefulWidget {
   const DebugScreen({super.key});
@@ -12,7 +13,10 @@ class _DebugScreenState extends State<DebugScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Debug Screen')),
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: []),
+      body: Center(child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: SizedBox(height: 200, child: BarChartSample()),
+      )),
     );
   }
 }
