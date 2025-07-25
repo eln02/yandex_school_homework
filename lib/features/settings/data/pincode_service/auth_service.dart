f326bc83fcf61134b98cfb3bfb78ca61e162b8d7
Елена <99683487+eln02@users.noreply.github.com>
2025-07-25 20:26:32 +0300

    fix

--- lib/features/settings/data/pincode_service/auth_service.dart
+++ lib/features/settings/data/pincode_service/auth_service.dart
@@ -1,18 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
 import 'package:yandex_school_homework/features/settings/data/pincode_service/i_auth_service.dart';
 
 class SecureAuthService implements IAuthService {
-  static const _pinKey = 'user_pin_hash';
-  static const _biometricFlagKey = 'biometric_enabled';
+  @visibleForTesting
+  static const pinKey = 'user_pin_hash';
+  @visibleForTesting
+  static const biometricFlagKey = 'biometric_enabled';
 
   final FlutterSecureStorage _secureStorage;
   bool _isPinSet = false;
   bool _isBiometricEnabled = false;
 
   SecureAuthService({required FlutterSecureStorage secureStorage})
-      : _secureStorage = secureStorage;
+    : _secureStorage = secureStorage;
 
   @override
   bool get isPinSet => _isPinSet;
@@ -20,7 +23,8 @@ class SecureAuthService implements IAuthService {
   @override
   bool get isBiometricEnabledCached => _isBiometricEnabled;
 
-  String _hashPin(String pin) {
+  @visibleForTesting
+  String hashPin(String pin) {
     final bytes = utf8.encode(pin);
     final digest = sha256.convert(bytes);
     return digest.toString();
@@ -28,27 +32,27 @@ class SecureAuthService implements IAuthService {
 
   @override
   Future<void> init() async {
-    _isPinSet = await _secureStorage.containsKey(key: _pinKey);
-    final biometricValue = await _secureStorage.read(key: _biometricFlagKey);
+    _isPinSet = await _secureStorage.containsKey(key: pinKey);
+    final biometricValue = await _secureStorage.read(key: biometricFlagKey);
     _isBiometricEnabled = biometricValue == 'true';
   }
 
   @override
   Future<void> savePin(String pin) async {
-    final hashedPin = _hashPin(pin);
-    final exists = await _secureStorage.containsKey(key: _pinKey);
+    final hashedPin = hashPin(pin);
+    final exists = await _secureStorage.containsKey(key: pinKey);
     if (exists) {
       throw Exception("PIN already exists. Use updatePin to change it.");
     }
-    await _secureStorage.write(key: _pinKey, value: hashedPin);
+    await _secureStorage.write(key: pinKey, value: hashedPin);
     _isPinSet = true;
   }
 
   @override
   Future<bool> validatePin(String pin) async {
-    final storedHash = await _secureStorage.read(key: _pinKey);
+    final storedHash = await _secureStorage.read(key: pinKey);
     if (storedHash == null) return false;
-    return storedHash == _hashPin(pin);
+    return storedHash == hashPin(pin);
   }
 
   @override
@@ -57,20 +61,20 @@ class SecureAuthService implements IAuthService {
     if (!isValid) {
       throw Exception("Old PIN is incorrect.");
     }
-    final newHashedPin = _hashPin(newPin);
-    await _secureStorage.write(key: _pinKey, value: newHashedPin);
+    final newHashedPin = hashPin(newPin);
+    await _secureStorage.write(key: pinKey, value: newHashedPin);
   }
 
   @override
   Future<void> deletePin() async {
-    await _secureStorage.delete(key: _pinKey);
+    await _secureStorage.delete(key: pinKey);
     _isPinSet = false;
   }
 
   @override
   Future<void> setBiometricEnabled(bool enabled) async {
     await _secureStorage.write(
-      key: _biometricFlagKey,
+      key: biometricFlagKey,
       value: enabled.toString(),
     );
     _isBiometricEnabled = enabled;
